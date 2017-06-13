750decafe5fcf46e21810d1654ab3298b844fbc3
JQDOM <Seattlepowers@gmail.com>
2017-06-13 08:34:10 -0700

    turtlegit add .git add .

--- Programming Foundations with Python/rename_love_letter.py
+++ Programming Foundations with Python/rename_love_letter.py
@@ -2,12 +2,12 @@ import os
 import string
 
 def rename_files():
-    file_list= os.listdir(r"C:\Users\Dragonslayer\Desktop\udacity\Programming Foundations with Python\prank\prank")
+    file_list= os.listdir(r"C:\Users\Dragonslayer\Desktop\udacity\Programming Foundations with Python\alphabet\alphabet")
     print(file_list)
     saved_path = os.getcwd()
     print(saved_path)
     print(os.getcwd())
-    os.chdir(r"C:\Users\Dragonslayer\Desktop\udacity\Programming Foundations with Python\prank\prank")
+    os.chdir(r"C:\Users\Dragonslayer\Desktop\udacity\Programming Foundations with Python\alphabet\alphabet")
     print(os.getcwd())
     for file_name in file_list:
         print("old name of file" + file_name)
