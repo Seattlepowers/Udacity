f373fc17ba3413970260139f984b15c741be7e78
JQDOM <Seattlepowers@gmail.com>
2017-06-13 12:05:22 -0700

    add sqaure, circle and triangle

--- draw_sqaure.py
+++ draw_sqaure.py
@@ -5,14 +5,34 @@ def draw_square():
     window.bgcolor("red")
 
     brad = turtle.Turtle()
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    brad.shape("turtle")
+    brad.color("blue")
+    brad.speed(5)
+    count = 0
+    square = 4
+    while count <= square:
+        brad.right(90)
+        brad.forward(100)
+        count = count +1
+
+    adam =turtle.Turtle()
+    adam.speed(10)
+    adam.shape("arrow")
+    adam.forward(100)
+    adam.circle(100)
+
+    angie =turtle.Turtle()
+    angie.speed(10)
+    angie.shape("arrow")
+    angie.color("white")
+    count = 0
+    triangle = 3
+    
+    while count < triangle:
+        angie.speed(10)
+        angie.forward(200)
+        angie.right(240)
+        count += 1
 
     window.exitonclick()
 
